b007230bb6fceef67a2f37b49998b45282ea5fca
Jamie Evans <jamieevans96@googlemail.com>
2020-06-03 09:47:48 +0100

    needs small tweaks

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const gameBoard = (() => {
     {grid: 'i', move: ""}];
 
   const render = () => {
+    document.querySelector(".startContainer").style.display = ("none");
+
+    document.querySelector(".winner").innerHTML = "";
+
     var temp = document.getElementById("container");
     while (temp.hasChildNodes()) {
       temp.removeChild(temp.firstChild);
@@ -57,7 +61,20 @@ function playerSubmit() {
   player2 = playerFactory(document.querySelector("#p2").value, 'o');
 
   document.querySelector("#playerForm").style.display = ("none");
+  document.querySelector(".startContainer").style.display = ("block");
   document.querySelector("#container").style.display = ("grid");
+}
+
+function p1Win() {
+  document.querySelector(".winner").innerHTML = player1.name + " Wins!";
+  document.querySelector(".startButton").innerHTML = "Restart Game";
+  document.querySelector(".startContainer").style.display = ("block");
+}
+
+function p2Win() {
+  document.querySelector(".winner").innerHTML = player1.name + " Wins!";
+  document.querySelector(".startButton").innerHTML = "Restart Game";
+  document.querySelector(".startContainer").style.display = ("block");
 }
 
 function checkGame() {
@@ -66,52 +83,58 @@ function checkGame() {
   }
 
   if (gameBoard.gameBoardArr[0].move == 'x' && gameBoard.gameBoardArr[1].move == 'x' && gameBoard.gameBoardArr[2].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[3].move == 'x' && gameBoard.gameBoardArr[4].move == 'x' && gameBoard.gameBoardArr[5].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[6].move == 'x' && gameBoard.gameBoardArr[7].move == 'x' && gameBoard.gameBoardArr[8].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[0].move == 'x' && gameBoard.gameBoardArr[3].move == 'x' && gameBoard.gameBoardArr[6].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[1].move == 'x' && gameBoard.gameBoardArr[4].move == 'x' && gameBoard.gameBoardArr[7].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[2].move == 'x' && gameBoard.gameBoardArr[5].move == 'x' && gameBoard.gameBoardArr[8].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[0].move == 'x' && gameBoard.gameBoardArr[4].move == 'x' && gameBoard.gameBoardArr[8].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
   else if (gameBoard.gameBoardArr[2].move == 'x' && gameBoard.gameBoardArr[4].move == 'x' && gameBoard.gameBoardArr[6].move == 'x') {
-    alert(player1.name + " wins!");
+    p1Win();
   }
 
   if (gameBoard.gameBoardArr[0].move == 'o' && gameBoard.gameBoardArr[1].move == 'o' && gameBoard.gameBoardArr[2].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[3].move == 'o' && gameBoard.gameBoardArr[4].move == 'o' && gameBoard.gameBoardArr[5].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[6].move == 'o' && gameBoard.gameBoardArr[7].move == 'o' && gameBoard.gameBoardArr[8].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[0].move == 'o' && gameBoard.gameBoardArr[3].move == 'o' && gameBoard.gameBoardArr[6].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[1].move == 'o' && gameBoard.gameBoardArr[4].move == 'o' && gameBoard.gameBoardArr[7].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[2].move == 'o' && gameBoard.gameBoardArr[5].move == 'o' && gameBoard.gameBoardArr[8].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[0].move == 'o' && gameBoard.gameBoardArr[4].move == 'o' && gameBoard.gameBoardArr[8].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
   }
   else if (gameBoard.gameBoardArr[2].move == 'o' && gameBoard.gameBoardArr[4].move == 'o' && gameBoard.gameBoardArr[6].move == 'o') {
-    alert(player2.name + " wins!");
+    p2Win();
+  }
+
+  else if (gameBoard.gameBoardArr[0].move != '' && gameBoard.gameBoardArr[1].move != '' && gameBoard.gameBoardArr[2].move != '' && gameBoard.gameBoardArr[3].move != '' && gameBoard.gameBoardArr[4].move != '' && gameBoard.gameBoardArr[5].move != '' && gameBoard.gameBoardArr[6].move != '' && gameBoard.gameBoardArr[7].move != '' && gameBoard.gameBoardArr[8].move != '') {
+    document.querySelector(".winner").innerHTML = "Draw!";
+    document.querySelector(".startButton").innerHTML = "Restart Game";
+    document.querySelector(".startContainer").style.display = ("block");
   }
 }
